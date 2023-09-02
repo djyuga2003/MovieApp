eeef5c748b0e888d7bbe73376daccfb80c1c5537
Rishi <srishi@example.com>
2023-09-02 20:36:12 +0530

    MovieFirstCommit
    some description

---
+++ src/main/java/dev/rishi/movies/Revivew.java
@@ -0,0 +1,19 @@
+package dev.rishi.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Revivew {
+
+    @Id
+    private ObjectId Id;
+    private String body;
+}
